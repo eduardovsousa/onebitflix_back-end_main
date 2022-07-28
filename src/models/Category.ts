fdb6607c0f0d694146084cbd25d230c18fab3202
eduardovsousa <eduardo.varela@outlook.com.br>
2022-07-28 15:38:07 -0300

    Criando a tabela de episódios

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -9,9 +9,9 @@ export interface Category {
 
 export interface CategoryCreationAttributes extends Optional<Category, 'id'> { }
 
-export interface CategoryInstace extends Model<Category, CategoryCreationAttributes>, Category { }
+export interface CategoryInstance extends Model<Category, CategoryCreationAttributes>, Category { }
 
-export const Category = sequelize.define<CategoryInstace, Category>('Category', {
+export const Category = sequelize.define<CategoryInstance, Category>('Category', {
     id: {
         allowNull: false,
         autoIncrement: true,
