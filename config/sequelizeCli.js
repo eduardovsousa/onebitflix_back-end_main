9cab6ae8de745069a8d9fd64b2a1325c6530f60e
eduardovsousa <eduardo.varela@outlook.com.br>
2022-07-28 03:40:33 -0300

    Configurando o Sequelize

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'onebitflix_db',
+        username: 'postgres',
+        password: '123123'
+    }
+}
