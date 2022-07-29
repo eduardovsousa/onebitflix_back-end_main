f7595aff15e6fc807ccfc8c1b85f25f42373c9e4
eduardovsousa <eduardo.varela@outlook.com.br>
2022-07-28 21:53:01 -0300

    Obtendo cursos em destaque (3)

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,7 +2,21 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
-    //Get /courses/:id
+    //GET /courses/featured
+    featured: async(req: Request, res: Response) => {
+
+        try {
+            const featuredCourses = await courseService.getRandomFeaturedCourses()
+            return res.json(featuredCourses)
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+
+    //GET /courses/:id
     show: async(req: Request, res: Response) => {
         const { id } = req.params
 
