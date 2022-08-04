709af5968f522c734ccb71fed129dd9c06c65021
eduardovsousa <eduardo.varela@outlook.com.br>
2022-08-04 01:43:04 -0300

    Obtendo os 10 cursos mais populares

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -3,7 +3,7 @@ import { getPaginationParams } from "../helpers/getPaginationParams"
 import { Course } from "../models"
 
 export const courseService = {
-    findByIdWithEpisodes: async(id: string) => {
+    findByIdWithEpisodes: async (id: string) => {
         const courseWithEpisodes = await Course.findByPk(id, {
             attributes: [
                 'id',
@@ -29,7 +29,7 @@ export const courseService = {
         return courseWithEpisodes
     },
 
-    getRandomFeaturedCourses: async() => {
+    getRandomFeaturedCourses: async () => {
         const featuredCourses = await Course.findAll({
             attributes: [
                 'id',
@@ -47,7 +47,7 @@ export const courseService = {
         return randomFeaturedCourses.slice(0, 3)
     },
 
-    getTopTenNewest: async() => {
+    getTopTenNewest: async () => {
         const courses = await Course.findAll({
             limit: 10,
             order:[['created_at', 'DESC']]
@@ -56,7 +56,34 @@ export const courseService = {
         return courses
     },
 
-    findByName: async(name: string, page: number, perPage: number) => {
+    getTopTenByLikes: async () => {
+        const results = await Course.sequelize?.query(
+          `SELECT
+            courses.id,
+            courses.name,
+            courses.synopsis,
+            courses.thumbnail_url as thumbnailUrl,
+            COUNT(users.id) AS likes
+          FROM courses
+            LEFT OUTER JOIN likes
+              ON courses.id = likes.course_id
+              INNER JOIN users
+                ON users.id = likes.user_id
+          GROUP BY courses.id
+          ORDER BY likes DESC
+          LIMIT 10;`
+        )
+    
+        if (results) {
+          const [topTen, metada] = results
+          return topTen
+        } else {
+          return null
+        }
+      },
+    
+
+    findByName: async (name: string, page: number, perPage: number) => {
         const offset = (page - 1) * perPage
 
         const { count, rows } = await Course.findAndCountAll({
